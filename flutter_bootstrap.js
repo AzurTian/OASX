ab1c76745ccca60b69f9269db26a6ac7d831dfe0
AzurTian <azurtian@qq.com>
2026-06-01 13:05:37 +0000

    Deploying to page from @ AzurTian/OASX@73adbe30a914bbe92bb4909502b3998e0a716cdd 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2840786047"
+    serviceWorkerVersion: "1789379243"
   }
 });
